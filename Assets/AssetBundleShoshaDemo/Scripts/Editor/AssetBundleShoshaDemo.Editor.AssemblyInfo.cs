ae4d90e5ccb86e770fec3683d48bdfca47910d99
eral <eral.r.l+github.com@gmail.com>
2019-02-03 03:47:08 +0900

    :tv:Demo:Add assembly info
    Demo:アセンブリ情報追加

---
+++ Assets/AssetBundleShoshaDemo/Scripts/Editor/AssetBundleShoshaDemo.Editor.AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("AssetBundleShoshaDemo.Scripts.Editor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssetBundleShoshaDemo.Scripts.Editor")]
+[assembly: AssemblyCopyright("Copyright © 2018 ERAL")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("f448820a-204c-4ccf-a476-a59b708fbc4d")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+//[assembly: AssemblyFileVersion("0.0.0.0")]
