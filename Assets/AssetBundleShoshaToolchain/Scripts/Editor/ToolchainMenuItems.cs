909e56f0f56430e41d40a7a3fbf3b80170358d89
eral <eral.r.l+github.com@gmail.com>
2019-02-03 03:52:31 +0900

    :thumbsup:Toolchain:refer AssemblyVersion
    Toolchain:アセンブリバージョンを参照する様に修正

--- Assets/AssetBundleShoshaToolchain/Scripts/Editor/ToolchainMenuItems.cs
+++ Assets/AssetBundleShoshaToolchain/Scripts/Editor/ToolchainMenuItems.cs
@@ -5,6 +5,7 @@ namespace AssetBundleShoshaToolchain.Editor {
 	using UnityEngine;
 	using UnityEditor;
 	using System.IO;
+	using System.Reflection;
 
 	public static class ToolchainMenuItems {
 		#region Public methods
@@ -14,13 +15,11 @@ namespace AssetBundleShoshaToolchain.Editor {
 		/// </summary>
 		[MenuItem("AssetBundleShoshaToolchain/Export Package/AssetBundleShosha", false, 30010)]
 		public static void ExportPackageAssetBundleShosha() {
-			CreateReleaseDirectory();
-
-			var assetPathName = "Assets/AssetBundleShosha";
-			var version = AssetDatabase.LoadAssetAtPath<Version>("Assets/AssetBundleShoshaToolchain/Assets/AssetBundleShoshaVersion.asset").version;
-			var fileName = "Release/AssetBundleShosha-" + version + ".unitypackage";
-			var flags = ExportPackageOptions.Recurse;
-			AssetDatabase.ExportPackage(assetPathName, fileName, flags);
+			ExportPackage("Assets/AssetBundleShosha"
+						, "Release/AssetBundleShosha-{0}.unitypackage"
+						, "AssetBundleShosha"
+						, "AssetBundleShosha.Editor"
+						);
 		}
 
 		/// <summary>
@@ -28,27 +27,76 @@ namespace AssetBundleShoshaToolchain.Editor {
 		/// </summary>
 		[MenuItem("AssetBundleShoshaToolchain/Export Package/AssetBundleShoshaDemo", false, 30020)]
 		public static void ExportPackageAssetBundleShoshaDemo() {
-			CreateReleaseDirectory();
-
-			var assetPathName = "Assets/AssetBundleShoshaDemo";
-			var version = AssetDatabase.LoadAssetAtPath<Version>("Assets/AssetBundleShoshaToolchain/Assets/AssetBundleShoshaDemoVersion.asset").version;
-			var fileName = "Release/AssetBundleShoshaDemo-" + version + ".unitypackage";
-			var flags = ExportPackageOptions.Recurse;
-			AssetDatabase.ExportPackage(assetPathName, fileName, flags);
+			ExportPackage("Assets/AssetBundleShoshaDemo"
+						, "Release/AssetBundleShoshaDemo-{0}.unitypackage"
+						, "AssetBundleShoshaDemo"
+						, "AssetBundleShoshaDemo.Editor"
+						);
 		}
 
 		#endregion
 		#region Private methods
 
 		/// <summary>
-		/// Releaseディレクトリ作成
+		/// ディレクトリ作成
 		/// </summary>
-		private static void CreateReleaseDirectory() {
-			var fullPath = Application.dataPath + "/../Release";
-			if (!Directory.Exists(fullPath)) {
+		/// <param name="fullPath">作成するディレクトリのフルパス</param>
+		/// <param name="excludeLastName">最後の名前を除外</param>
+		public static void CreateDirectory(string fullPath, bool excludeLastName = false) {
+			if (excludeLastName) {
+				fullPath = Path.GetDirectoryName(fullPath);
+			}
+			if (Directory.Exists(fullPath)) {
+				//ディレクトリが在るなら
+				//empty.
+			} else {
+				//ディレクトリが無いなら
+				var parentFullPath = Path.GetDirectoryName(fullPath);
+				if (Directory.Exists(parentFullPath)) {
+					//親ディレクトリが無いなら
+					CreateDirectory(parentFullPath, false);
+				}
 				Directory.CreateDirectory(fullPath);
 			}
 		}
+
+		/// <summary>
+		/// アセンブリバージョンの取得
+		/// </summary>
+		/// <param name="assemblyName">アセンブリ名</param>
+		/// <returns>バージョン</returns>
+		private static System.Version GetAssemblyVersion(string assemblyName) {
+			return Assembly.Load(assemblyName).GetName().Version;
+		}
+
+		/// <summary>
+		/// パッケージエクスポート
+		/// </summary>
+		/// <param name="assetPathName">エクスポートするアセットのパス</param>
+		/// <param name="filePath">エクスポート先(「{0}」にバージョンが挿入される)</param>
+		/// <param name="runtimeAssemblyName">ランタイムアセンブリ名</param>
+		/// <param name="editorAssemblyName">エディタアセンブリ名</param>
+		/// <returns>true:成功、false:失敗</returns>
+		private static bool ExportPackage(string assetPathName, string filePath, string runtimeAssemblyName, string editorAssemblyName) {
+			var result = true;
+			var version = GetAssemblyVersion(runtimeAssemblyName);
+			var editorVersion = GetAssemblyVersion(editorAssemblyName);
+			if (version != editorVersion) {
+				var message = "ランタイムアセンブリとエディタアセンブリのバージョンが違います。\nエクスポートしますか?\n\nランタイムアセンブリバージョン:" + version + "\nエディタアセンブリバージョン　:" + editorVersion;
+				if (!EditorUtility.DisplayDialog("アセンブリバージョンの不一致", message, "Export", "Cancel")) {
+					result = false;
+				}
+			}
+			if (result) {
+				var fileName = string.Format(filePath, version);
+				var fullPath = Application.dataPath + "/../" + fileName;
+				CreateDirectory(fullPath, true);
+				var flags = ExportPackageOptions.Recurse;
+				AssetDatabase.ExportPackage(assetPathName, fileName, flags);
+			}
+			return result;
+		}
+
 		#endregion
 	}
 }
