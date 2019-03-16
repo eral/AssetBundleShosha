ad31a95c037c0a94cd8e8166b9b2301b72735e80
eral <eral.r.l+github.com@gmail.com>
2019-03-17 03:21:30 +0900

    :heavy_plus_sign:AssetBundleCrypto:Support load from stream
    AssetBundleCrypto:ストリーム読み込み対応

--- Assets/AssetBundleShosha/Scripts/Internal/AssetBundleDecryptoStream.cs
+++ Assets/AssetBundleShosha/Scripts/Internal/AssetBundleDecryptoStream.cs
@@ -13,37 +13,44 @@ namespace AssetBundleShosha.Internal {
 		/// 現在位置
 		/// </summary>
 		public override long Position {get{
-			return m_Decrypto.Position;
+			return m_DecryptoStreamPosition;
 		}set{
-			m_Decrypto.Position = value;
+			if (value < 0) {
+				throw new System.IO.IOException();
+			}
+			if (value < m_DecryptoStreamPosition) {
+				ResetDecryptoStream();
+			}
+			var offset = (int)(value - m_DecryptoStreamPosition);
+			ReadDecryptoStream(offset);
 		}}
 
 		/// <summary>
 		/// ストリーム長
 		/// </summary>
 		public override long Length {get{
-			return m_Decrypto.Length;
+			return m_DecryptoStream.Length;
 		}}
 
 		/// <summary>
 		/// 書き込み可能確認
 		/// </summary>
 		public override bool CanWrite {get{
-			return m_Decrypto.CanWrite;
+			return m_DecryptoStream.CanWrite;
 		}}
 
 		/// <summary>
 		/// シーク可能確認
 		/// </summary>
 		public override bool CanSeek {get{
-			return m_Decrypto.CanSeek;
+			return true;
 		}}
 
 		/// <summary>
 		/// 読み出し可能確認
 		/// </summary>
 		public override bool CanRead {get{
-			return m_Decrypto.CanRead;
+			return m_DecryptoStream.CanRead;
 		}}
 
 		#endregion
@@ -53,14 +60,16 @@ namespace AssetBundleShosha.Internal {
 		/// クローズ
 		/// </summary>
 		public override void Close() {
-			m_Decrypto.Close();
+			m_DecryptoStream.Close();
+			m_Source.Close();
 		}
 
 		/// <summary>
 		/// フラッシュ
 		/// </summary>
 		public override void Flush() {
-			m_Decrypto.Flush();
+			m_DecryptoStream.Flush();
+			m_Source.Flush();
 		}
 
 		/// <summary>
@@ -71,7 +80,9 @@ namespace AssetBundleShosha.Internal {
 		/// <param name="count">読み込みサイズ</param>
 		/// <returns></returns>
 		public override int Read(byte[] buffer, int offset, int count) {
-			return m_Decrypto.Read(buffer, offset, count);
+			var result = m_DecryptoStream.Read(buffer, offset, count);
+			m_DecryptoStreamPosition += result;
+			return result;
 		}
 
 		/// <summary>
@@ -81,7 +92,20 @@ namespace AssetBundleShosha.Internal {
 		/// <param name="origin">基準点</param>
 		/// <returns>現在位置</returns>
 		public override long Seek(long offset, SeekOrigin origin) {
-			return m_Decrypto.Seek(offset, origin);
+			long position = 0;
+			switch (origin) {
+			case SeekOrigin.Begin:
+				position = offset;
+				break;
+			case SeekOrigin.Current:
+				position = Position + offset;
+				break;
+			case SeekOrigin.End:
+				position = Length + offset;
+				break;
+			}
+			Position = position;
+			return position;
 		}
 
 		/// <summary>
@@ -89,7 +113,7 @@ namespace AssetBundleShosha.Internal {
 		/// </summary>
 		/// <param name="value">ストリーム長</param>
 		public override void SetLength(long value) {
-			m_Decrypto.SetLength(value);
+			m_DecryptoStream.SetLength(value);
 		}
 
 		/// <summary>
@@ -99,7 +123,8 @@ namespace AssetBundleShosha.Internal {
 		/// <param name="offset">書き込み元のオフセット</param>
 		/// <param name="count">書き込みサイズ</param>
 		public override void Write(byte[] buffer, int offset, int count) {
-			m_Decrypto.Write(buffer, offset, count);
+			m_DecryptoStream.Write(buffer, offset, count);
+			m_DecryptoStreamPosition += count;
 		}
 
 		/// <summary>
@@ -108,30 +133,24 @@ namespace AssetBundleShosha.Internal {
 		/// <param name="source">元データ</param>
 		/// <param name="cryptoHash">暗号化ハッシュ</param>
 		public AssetBundleDecryptoStream(Stream source, int cryptoHash) {
+			m_Source = source;
+			m_Rijndael = GetAES128Rijndael();
+
 			var iv = new byte[AssetBundleCrypto.kIVSize];
 			source.Read(iv, 0, iv.Length);
 			var fileSizeBytes = new byte[sizeof(int)];
 			source.Read(fileSizeBytes, 0, fileSizeBytes.Length);
-			var fileSize = System.BitConverter.ToInt32(fileSizeBytes, 0);
-			using (var destStream = new MemoryStream(fileSize)) {
-				var rijndael = GetAES128Rijndael();
-				const int kCryptoBufferSize = 16 * 1024;
+			var key = AssetBundleCrypto.GetCryptoKey(cryptoHash);
 
-				var key = AssetBundleCrypto.GetCryptoKey(cryptoHash);
-				var decryptor = rijndael.CreateDecryptor(key, iv);
-				var buffer = new byte[kCryptoBufferSize];
-				using (var cryptoStream = new CryptoStream(source, decryptor, CryptoStreamMode.Read)) {
-					while (true) {
-						var readed = cryptoStream.Read(buffer, 0, buffer.Length);
-						if (0 == readed) {
-							break;
-						}
-						destStream.Write(buffer, 0, readed);
-					}
-				}
+			m_CreateDecryptoStream = ()=>{
+				var decryptor = m_Rijndael.CreateDecryptor(key, iv);
+				m_Source.Position = kCryptoHeaderSize;
+				return new CryptoStream(m_Source, decryptor, CryptoStreamMode.Read);
+			};
 
-				m_Decrypto = new MemoryStream(destStream.GetBuffer(), false);
-			}
+			m_ReadBuffer = new byte[kReadBufferSize];
+
+			ResetDecryptoStream();
 		}
 
 		#endregion
@@ -142,9 +161,25 @@ namespace AssetBundleShosha.Internal {
 		/// </summary>
 		/// <param name="disposing">true:Dispose, false:Finalizer</param>
 		protected override void Dispose(bool disposing) {
-			m_Decrypto.Dispose();
+			m_CreateDecryptoStream = null;
+			((System.IDisposable)m_Rijndael).Dispose();
+			m_DecryptoStream.Dispose();
+			m_Source.Dispose();
 			base.Dispose(disposing);
 		}
+
+		#endregion
+		#region Private const fields
+
+		/// <summary>
+		/// 暗号化ヘッダーサイズ
+		/// </summary>
+		private int kCryptoHeaderSize = sizeof(byte) * AssetBundleCrypto.kIVSize + sizeof(int);
+
+		/// <summary>
+		/// 読み込みバッファサイズ
+		/// </summary>
+		private int kReadBufferSize = 10 * 1024;
 
 		#endregion
 		#region Private fields and properties
@@ -152,15 +187,61 @@ namespace AssetBundleShosha.Internal {
 		/// <summary>
 		/// ソース
 		/// </summary>
-		private Stream m_Decrypto;
+		private Stream m_Source;
 
 		/// <summary>
-		/// 暗号化ハッシュ
+		/// 復号ストリーム
 		/// </summary>
-		private int m_CryptoHash;
+		private Stream m_DecryptoStream;
+
+		/// <summary>
+		/// 復号ストリーム現在位置
+		/// </summary>
+		private long m_DecryptoStreamPosition;
+
+		/// <summary>
+		/// 復号ストリーム作成
+		/// </summary>
+		private System.Func<Stream> m_CreateDecryptoStream;
+
+		/// <summary>
+		/// Rijndael
+		/// </summary>
+		private RijndaelManaged m_Rijndael;
+
+		/// <summary>
+		/// 読み込みバッファ
+		/// </summary>
+		private byte[] m_ReadBuffer;
 
 		#endregion
 		#region Private methods
+
+		/// <summary>
+		/// 復号ストリームの初期化
+		/// </summary>
+		private void ResetDecryptoStream() {
+			m_DecryptoStream = m_CreateDecryptoStream();
+			m_DecryptoStreamPosition = 0;;
+		}
+
+		/// <summary>
+		/// 復号ストリームの読み込み
+		/// </summary>
+		/// <param name="count">読み込み料</param>
+		private void ReadDecryptoStream(int count) {
+			if (0 < count) {
+				do {
+					var readSize = ((count < m_ReadBuffer.Length)? count: m_ReadBuffer.Length);
+					readSize = m_DecryptoStream.Read(m_ReadBuffer, 0, readSize);
+					if (readSize == 0) {
+						break;
+					}
+					count -= readSize;
+					m_DecryptoStreamPosition += readSize;
+				} while (0 < count);
+			}
+		}
 
 		/// <summary>
 		/// AES128設定のRijndael取得
